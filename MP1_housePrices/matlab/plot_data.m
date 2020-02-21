0a482405ca1a146416466375902635aaa867af11
PascalPolygon <pdao2015@my.fit.edu>
2020-02-21 00:44:13 -0500

    Corrected plots

--- MP1_housePrices/matlab/plot_data.m
+++ MP1_housePrices/matlab/plot_data.m
@@ -1,9 +1,9 @@
 Array=csvread("../dataset/housing_dataset_matlab.csv");
 
-fields = ["SalePrice", "LotFrontage", "OverallQual", "MasVnrArea", "YearBuilt", "BsmtUnfSF", "YearRemodAdd", "TotalBsmtSF", "BsmtFinSF1", "1stFlrSF"];
+fields = ["SalePrice","LotFrontage",  "LotArea", "OverallQual", "MasVnrArea", "YearBuilt", "BsmtUnfSF", "YearRemodAdd", "TotalBsmtSF", "BsmtFinSF1", "1stFlrSF"];
 col1 = Array(:,1);
 
-for i = 1:10
+for i = 2:11
     col2 = Array(:,i);
 
     scatter(col1, col2);
