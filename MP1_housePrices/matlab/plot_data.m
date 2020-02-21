fc993011a4b7bc8ac9552870fb2d25824dcbc703
PascalPolygon <pdao2015@my.fit.edu>
2020-02-21 00:38:33 -0500

    Plots with matlab

--- MP1_housePrices/matlab/plot_data.m
+++ MP1_housePrices/matlab/plot_data.m
@@ -1,6 +1,15 @@
 Array=csvread("../dataset/housing_dataset_matlab.csv");
+
+fields = ["SalePrice", "LotFrontage", "OverallQual", "MasVnrArea", "YearBuilt", "BsmtUnfSF", "YearRemodAdd", "TotalBsmtSF", "BsmtFinSF1", "1stFlrSF"];
 col1 = Array(:,1);
-for i = 1:11
+
+for i = 1:10
     col2 = Array(:,i);
-    saveas(scatter(col1, col2), strcat("scatter_",int2str(i),".png"));
+
+    scatter(col1, col2);
+    title(strcat(fields{i}," Vs. ",fields{1}));
+    xlabel(fields{i})
+    ylabel(fields{1})
+    saveas(gcf, strcat("../figures/",fields{i},".jpg"));
+    
 end
